ae4dbbdc8b97f1654ed2a6435ea0b1ad606eae19
goughy12 <9194460+goughy12@users.noreply.github.com>
2025-04-06 20:19:29 +0800

    Update Get-GithubRelease.ps1

--- Get-GithubRelease.ps1
+++ Get-GithubRelease.ps1
@@ -162,7 +162,7 @@ function Get-GithubRelease {
             else {
                 Write-Host "[ERROR] Multiple assets found matching '$Asset':`n" 
             }
-            Write-Host "   Repo:           $($Repo):"
+            Write-Host "   Repo:           $($Repo)"
             Write-Host "   ReleaseTag:     $($release.tag_name)`n"
             Write-Host "   Available assets:`n"
             foreach ($item in $release.assets) {
